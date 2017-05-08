2f62706a8e9fb164fec1476e91ae3a8df1bf73f7
Michael Prokofyev <mikeprokofyev@gmail.com>
2017-05-08 03:14:27 +0300

    basic laser reflections

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour {
+
+    [SerializeField]
+    float raycastDistance = 5f;
+
+    LineRenderer laserLine;
+
+	// Use this for initialization
+	void Start () {
+        laserLine = GetComponent<LineRenderer>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 startPosition = transform.position;
+        Vector3 endPosition = startPosition + Random.insideUnitSphere * 5f;
+
+        laserLine.SetPosition(0, startPosition);
+
+        //Try hitting stuff
+        RaycastHit hitInfo;
+
+        Vector3 direction = endPosition - startPosition;
+
+        bool hitSomething = Physics.Raycast(transform.position, direction, out hitInfo, raycastDistance);
+        if(hitSomething) {
+            laserLine.positionCount = 3;
+          //  print(hitInfo.point);
+            laserLine.SetPosition(1, hitInfo.point);
+            //Reflection
+            Vector3 pos = Vector3.Reflect(direction * raycastDistance, hitInfo.normal);
+            laserLine.SetPosition(2, pos);
+        }
+        else {
+            laserLine.positionCount = 2;
+            laserLine.SetPosition(1, endPosition);
+        }
+
+    }
+}
