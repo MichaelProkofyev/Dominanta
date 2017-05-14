2f9219249363883e4147f8acde0dfefbc8cad763
Sergey Blagodetelev <s@blagodetelev.ru>
2017-05-14 22:54:17 +0300

    Обновился до 2017

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class Laser : MonoBehaviour {
 
     [SerializeField]
     float raycastDistance = 5f;
 
     LineRenderer laserLine;
+
+    public Vector3 startPos;
+    public Vector3 endPos;
 
 	// Use this for initialization
 	void Start () {
@@ -18,8 +22,13 @@ public class Laser : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        Vector3 startPosition = transform.position;
-        Vector3 endPosition = startPosition + Random.insideUnitSphere * 5f;
+//        Vector3 startPosition = transform.position;
+//        Vector3 endPosition = startPosition + Random.insideUnitSphere * 5f;
+
+        Vector3 startPosition = startPos;
+        Vector3 endPosition = endPos;
+
+
 
         laserLine.SetPosition(0, startPosition);
 
